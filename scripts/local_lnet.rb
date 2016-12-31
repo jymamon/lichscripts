0bdff45843cf5732bf5ca443345741b8599f0503
Jymamon <gs4-jymamon@hotmail.com>
2016-12-30 17:47:47 -0800

    local_lnet: local DRb server for characters.

---
+++ scripts/local_lnet.rb
@@ -0,0 +1,77 @@
+=begin
+    Local character DRb server
+
+    ;local_lich --help
+
+    author: Jymamon (gs4-jymamon@hotmail.com)
+       game: Gemstone
+    version: 2015.06.12.01
+
+    history:
+=end
+
+require 'drb'
+
+SERVER_URI="druby://localhost:20002"
+
+class Characters
+    include DRbUndumped
+    attr_reader :character_hash
+
+    def initialize
+        @character_hash = Hash.new
+    end
+
+    # Causes group members to be accessed and the failures handled so
+    # we don't need to do the same in all methods. May be a perf issue
+    # and need to be individually inlined later.
+    def check
+        @character_hash.each_pair { |name,object|
+            begin
+                if ( "#{name.downcase}"!="#{object.name.downcase}" )
+                    @character_hash.delete(name)
+                end
+            rescue
+                @character_hash.delete(name)
+            end
+        }
+        return(true)
+    end
+
+    def add_character(character)
+        check()
+
+        begin
+            @character_hash[character.name().downcase] = character
+        rescue
+            sleep 0.1
+            retry
+        end
+
+        return(@party)
+    end
+
+    def characters()
+        check()
+        return(@character_hash.keys)
+    end
+
+    def del_character(character_name)
+        check()
+        @character_hash.delete(character_name.downcase)
+        return(@party)
+    end
+
+    def members()
+        check()
+        return(@character_hash)
+    end
+
+    def size()
+        check()
+        return(@character_hash.size)
+    end
+end
+
+DRb.start_service SERVER_URI, Characters.new
+DRb.thread.join
